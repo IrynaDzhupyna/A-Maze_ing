d417fd9bade67f0c86799deff8b113842e991567
Iryna Dzhupyna <irdzhupy@c1b5c1.42berlin.de>
2026-05-07 20:31:42 +0200

    mazeGen

--- mazegen/maze.py
+++ mazegen/maze.py
@@ -104,7 +104,7 @@ class Maze:
                 ValueError; if position is invalid or out of bounds.
         """
         if not isinstance(pos, tuple) or len(pos) != 2:
-            raise ValueError(f"{name} must be a tuple {x, y}")
+            raise ValueError(f"{name} must be a tuple (x, y)")
         
         if not all(isinstance(coord, int) for coord in pos):
             raise ValueError(f"{name} must be integers")
@@ -113,153 +113,154 @@ class Maze:
         if not (0 <= x < width and 0 <= y < height):
             raise ValueError(f"{name} must be inside maze bounds")
         
-        def get_neighbors(self, cell):  # get the neighbor cell of the current cell
-            neighbors = []
+    def get_neighbors(self, cell):  # get the neighbor cell of the current cell
+        neighbors = []
 
-            directions = [  # pick one of the 4 directions
-                (0, -1, "N"),
-                (1, 0, "E"),
-                (0, 1, "S"),
-                (-1, 0, "W")
-            ]
+        directions = [  # pick one of the 4 directions
+            (0, -1, "N"),
+            (1, 0, "E"),
+            (0, 1, "S"),
+            (-1, 0, "W")
+        ]
 
-            for dx, dy, direction in directions:
-                nx = cell.x + dx
-                ny = cell.y + dy
+        for dx, dy, direction in directions:
+            nx = cell.x + dx
+            ny = cell.y + dy
 
-                if 0 <= nx < self.width and 0 <= ny < self.height:
-                    neighbor = self.grid[ny][nx]
+            if 0 <= nx < self.width and 0 <= ny < self.height:
+                neighbor = self.grid[ny][nx]
 
-                    if not neighbor.visited and (nx, ny) not in self.pattern_42:
-                        neighbors.append((neighbor, direction))
-
-            return neighbors
-        
-
-        def get_all_neighbors(self, cell):
-            neighbors = []
-
-            directions = [
-                (0, -1, "N"),
-                (1, 0, "E"),
-                (0, 1, "S"),
-                (-1, 0, "W")
-            ]
-
-            for dx, dy, direction in directions:
-                nx = cell.x + dx
-                ny = cell.y + dy
-
-                if 0 <= nx < self.width and 0 <= ny < self.height:
-                    neighbor = self.grid[ny][nx]
+                if not neighbor.visited and (nx, ny) not in self.pattern_42:
                     neighbors.append((neighbor, direction))
 
-            return neighbors
+        return neighbors
+    
+
+    def get_all_neighbors(self, cell):
+        neighbors = []
+
+        directions = [
+            (0, -1, "N"),
+            (1, 0, "E"),
+            (0, 1, "S"),
+            (-1, 0, "W")
+        ]
+
+        for dx, dy, direction in directions:
+            nx = cell.x + dx
+            ny = cell.y + dy
+
+            if 0 <= nx < self.width and 0 <= ny < self.height:
+                neighbor = self.grid[ny][nx]
+                neighbors.append((neighbor, direction))
+
+        return neighbors
+    
+
+    def generate(self):
+        stack = []
+        current = self.entry  # start at the entry coordinate
+        current.visited = True
+
+        while True:
+            neighbors = self.get_neighbors(current)
+
+            if neighbors:
+                next_cell, direction = random.choice(neighbors)
+
+                stack.append(current)
+
+                self.remove_wall(current, next_cell, direction)
+
+                next_cell.visited = True  # set the next cell as 'visited'
+                current = next_cell  # set the next cell as the current one
+
+            elif stack:
+                current = stack.pop()
+
+            else:
+                break
         
+        if self.perfect == False:
+            print("Imperfect maze")
 
-        def generate(self):
-            stack = []
-            current = self.entry  # start at the entry coordinate
-            current.visited = True
+            nb_walls_to_break = int((self.width * self.height) * 0.1)
+            print("number of walls to remove :", nb_walls_to_break)
 
-            while True:
-                neighbors = self.get_neighbors(current)
+            removed_wall = 0
 
-                if neighbors:
-                    next_cell, direction = random.choice(neighbors)
+            while removed_wall < nb_walls_to_break:
+                cell = self.get_cell(
+                    random.randint(0, self.width - 1),
+                    random.randint(0, self.height - 1)
+                )
+            
+                neighbors = self.get_all_neighbors(cell)
 
-                    stack.append(current)
+                valid_neighbors = [
+                    (n, d) for (n, d) in neighbors
+                    if cell.walls[d]
+                    and (cell.x, cell.y) not in self.pattern_42
+                    and (n.x, n.y) not in self.pattern_42
+                ]
 
-                    self.remove_wall(current, next_cell, direction)
+                if not valid_neighbors:
+                    continue
 
-                    next_cell.visited = True  # set the next cell as 'visited'
-                    current = next_cell  # set the next cell as the current one
+                neighbor, direction = random.choice(valid_neighbors)
 
-                elif stack:
-                    current = stack.pop()
+                if cell == self.entry or cell == self.exit:
+                    continue
+
+                if not (self.is_corridor(cell) or self.is_corridor(neighbor)):
+                    continue
 
                 else:
-                    break
-            if self.perfect_maze == "False":
-                print("Imperfect maze")
+                    self.remove_wall(cell, neighbor, direction)
+                    removed_wall += 1
+                    # print("wall removed")
 
-                nb_walls_to_break = int((self.width * self.height) * 0.1)
-                print("number of walls to remove :", nb_walls_to_break)
+    def is_corridor(self, cell):
+        open_walls = sum(not w for w in cell.walls.values())
+        return open_walls <= 2
+    
 
-                removed_wall = 0
+    def remove_wall(self, current, next_cell, direction):
+        # remove a wall from a cell
+        # and the wall from the other (opposite) cell
+        opposite = {"N": "S", "S": "N", "W": "E", "E": "W"}
 
-                while removed_wall < nb_walls_to_break:
-                    cell = self.get_cell(
-                        random.randint(0, self.width - 1),
-                        random.randint(0, self.height - 1)
-                    )
-                
-                    neighbors = self.get_all_neighbors(cell)
+        current.walls[direction] = False
+        # destroys one wall of the current cell
+        next_cell.walls[opposite[direction]] = False
+        # destroys the opposite wall of the next cell
 
-                    valid_neighbors = [
-                        (n, d) for (n, d) in neighbors
-                        if cell.walls[d]
-                        and (cell.x, cell.y) not in self.pattern_42
-                        and (n.x, n.y) not in self.pattern_42
-                    ]
+    def generate_pattern_42(self):
+        center_x = self.width // 2
+        center_y = self.height // 2
 
-                    if not valid_neighbors:
-                        continue
+        return [
+            (center_x - 3, center_y - 2),
+            (center_x - 3, center_y - 1),
+            (center_x - 3, center_y),
+            (center_x - 2, center_y),
+            (center_x - 1, center_y - 2),
+            (center_x - 1, center_y - 1),
+            (center_x - 1, center_y),
+            (center_x - 1, center_y + 1),
+            (center_x - 1, center_y + 2),
+            # '4'
 
-                    neighbor, direction = random.choice(valid_neighbors)
-
-                    if cell == self.entry or cell == self.exit:
-                        continue
-
-                    if not (self.is_corridor(cell) or self.is_corridor(neighbor)):
-                        continue
-
-                    else:
-                        self.remove_wall(cell, neighbor, direction)
-                        removed_wall += 1
-                        # print("wall removed")
-
-        def is_corridor(self, cell):
-            open_walls = sum(not w for w in cell.walls.values())
-            return open_walls <= 2
-        
-
-        def remove_wall(self, current, next_cell, direction):
-            # remove a wall from a cell
-            # and the wall from the other (opposite) cell
-            opposite = {"N": "S", "S": "N", "W": "E", "E": "W"}
-
-            current.walls[direction] = False
-            # destroys one wall of the current cell
-            next_cell.walls[opposite[direction]] = False
-            # destroys the opposite wall of the next cell
-
-        def generate_pattern_42(self):
-            center_x = self.width // 2
-            center_y = self.height // 2
-
-            return [
-                (center_x - 3, center_y - 2),
-                (center_x - 3, center_y - 1),
-                (center_x - 3, center_y),
-                (center_x - 2, center_y),
-                (center_x - 1, center_y - 2),
-                (center_x - 1, center_y - 1),
-                (center_x - 1, center_y),
-                (center_x - 1, center_y + 1),
-                (center_x - 1, center_y + 2),
-                # '4'
-
-                (center_x + 1, center_y - 2),
-                (center_x + 1, center_y),
-                (center_x + 1, center_y + 1),
-                (center_x + 1, center_y + 2),
-                (center_x + 2, center_y - 2),
-                (center_x + 2, center_y),
-                (center_x + 2, center_y + 2),
-                (center_x + 3, center_y - 2),
-                (center_x + 3, center_y - 1),
-                (center_x + 3, center_y),
-                (center_x + 3, center_y + 2),
-                # '2'
-            ]
+            (center_x + 1, center_y - 2),
+            (center_x + 1, center_y),
+            (center_x + 1, center_y + 1),
+            (center_x + 1, center_y + 2),
+            (center_x + 2, center_y - 2),
+            (center_x + 2, center_y),
+            (center_x + 2, center_y + 2),
+            (center_x + 3, center_y - 2),
+            (center_x + 3, center_y - 1),
+            (center_x + 3, center_y),
+            (center_x + 3, center_y + 2),
+            # '2'
+        ]
--- mazegen/maze_generator.py
+++ mazegen/maze_generator.py
@@ -5,7 +5,36 @@ from .maze import Maze
 #   - takes parameters, kicks off generation, exposes the result
 
 class MazeGenerator:
-    def __init__(self, width, height, entry_pos, exit_pos, perfect=True, seed=None)
+    # describes the object
+    #   what class represents, what used for, what it stores (attributes)
+    """
+        Generates a maze based on given parameters such as size,
+        entry/exit positions, and generation type(perfect/imperfect).
+         
+        Attributes:
+            width (int): width of maze
+            height (int): height of maze
+            entry_pos (tuple[int, int]): entry position as (x,y)
+            exit_pos(tuple[int, int]): exit position as (x,y)
+            perfect (bool, optional): whether the maze is perfect
+            seed (int|None): random seed
+         """
+    def __init__(self, width, height, entry_pos, exit_pos, perfect=True, seed=None):
+        # describes initalization (inputs)
+        #   what you passed in and what they mean
+        """
+        Initializes a MazeGenerator with configuration parameters.
+
+        Args:
+            width (int): amount of colums in maze
+            height (int): amount of rows in maze
+            entry_pos (tuple[int, int]): entry position as (x,y)
+            exit_pos(tuple[int, int]): exit position as (x,y)
+            perfect (bool, optional): if True, generates a perfect maze
+                                        (no loops, only one way path between two points).
+                                        Defaults to True
+                                    
+            seed (int|None): random seed for reproducible maze generation. If None, randomness is not fixed"""
         # stores the parameters
         self.width = width
         self.height = height
@@ -19,8 +48,32 @@ class MazeGenerator:
 #   - to set extra options before generating
 #   - or generate multiple mazes with the same generator 
 
-    def generate(self):
-        # makes randomness reproducible
+    def generate(self) -> None:
+        """
+            Generates a new maze instance using the current configuration.
+
+            This method initializes the random seed, creates a Maze object with the provided parameters,
+            and generates the maze structure.
+
+            Returns:
+                None
+        """
         random.seed(self.seed)
-        # builds the maze
-        self.maze = Maze(self.width, self.height, )
+        self.maze: Maze = Maze(self.width, self.height, self.entry_pos, self.exit_pos, self.perfect)
+        self.maze.generate()
+
+
+    def get_maze(self) -> Maze:
+        """
+        Returns the generated maze object
+
+        Returns: The generated maze
+
+        Raises:
+            ValueError if generate() has not been called yet. 
+        """
+        if self.maze is None:
+            raise ValueError("maze wasn't generated yet. Call generate() first")
+        return self.maze
+        
+        

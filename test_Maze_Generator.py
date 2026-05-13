3d72bbc69d8f300662bee8331084542e06194461
Iryna Dzhupyna <irdzhupy@c1b4c1.42berlin.de>
2026-05-13 12:43:45 +0200

    test

--- test_Maze_Generator.py
+++ test_Maze_Generator.py
@@ -24,12 +24,54 @@ def test_get_maze_without_generate_raises_error(maze_obj):
     (6, 9, (5, 7), (7, 3)),
     (6, 9, (5, 7), (5, 10))
 ])
+
 def test_invalid_parameters(width, height, entry_pos, exit_pos):
     with pytest.raises(ValueError):
-        MazeGenerator(
+        generator = MazeGenerator(
             width=width,
             height=height,
             entry_pos=entry_pos,
             exit_pos=exit_pos,
             seed=42
         )
+        generator.generate()
+
+# after generate maze correct dimensions
+def test_maze_correct_dimension(maze_generated):
+    maze = maze_generated.get_maze()
+    assert len(maze.grid) == 8
+    assert len(maze.grid[0]) == 10
+
+# all cells are reachable, except 42 pattern
+def test_all_cells_are_reachable(maze_get_maze):
+    maze = maze_get_maze
+    visited = set()
+    queue = [maze.entry]
+    visited.add((maze.entry.x, maze.entry.y))
+
+    directions = [
+        (0, -1 , "N"),
+        (1, 0, "E"),
+        (0, 1, "S"),
+        (-1 , 0, "W")
+
+    ]
+
+    while queue:
+        current = queue.pop(0)
+
+        for dx, dy, direction in directions:
+            nx = current.x + dx
+            ny = current.y + dy
+
+        if not current.walls[direction]:
+            if 0 <= nx <= maze.width and 0 <= ny <= maze.height:
+                if (nx, ny) not in visited:
+                    neighbor = maze.grid[ny][nx]
+                    queue.append(neighbor)
+                    visited.add((nx, ny))
+    assert len(visited) == maze.width * maze.height - len(maze.pattern_42)
+        
+        
+
+
